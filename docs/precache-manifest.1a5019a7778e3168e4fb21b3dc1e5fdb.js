ed8f570a81df53cbe52dd8cd24a367edcafe9cfa
Diego Paura <dpaura@gmail.com>
2020-07-08 17:41:32 -0300

    docs add

---
+++ docs/precache-manifest.1a5019a7778e3168e4fb21b3dc1e5fdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd554bdcf6549d0a6d39e5e94478433",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a22be59ce87292119774",
+    "url": "/static/css/main.29634db5.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a22be59ce87292119774",
+    "url": "/static/js/main.703b86f4.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
